68591cde3208c7c91e2501408d0bedce5b6210fa
Brunno Marchetti <BrunnoM7@users.noreply.github.com>
2021-12-19 15:03:58 -0300

    finished setting up the log reducer, actions and types

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechsListModal from './components/techs/TechListModal';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -19,17 +22,19 @@ const App = () => {
   }, []);
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechsListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechsListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
---
+++ src/actions/logActions.js
@@ -0,0 +1,45 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR
+} from './types';
+
+// export const getLogs = () => {
+//   return async (dispath) => {
+//     setLoading();
+
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+
+//     dispath({
+//       type: GET_LOGS,
+//       payload: data
+//     });
+//   }
+// }
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  } 
+}
+
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({ 
+      type: LOGS_ERROR,
+      payload: err.response.data
+    })
+  }
+}
