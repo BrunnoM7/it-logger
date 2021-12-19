b909be8e31d4ae7c415a9d9e1c13dae6bc5e61e1
Brunno Marchetti <BrunnoM7@users.noreply.github.com>
2021-12-19 16:40:26 -0300

    added update log and current actions to reducer

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from './types';
 
 // export const getLogs = () => {
@@ -90,4 +93,45 @@ export const deleteLog = id => async dispatch => {
       payload: err.response.data
     });
   }
+}
+
+// Update log on server
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`,{
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json()
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    })
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+}
+
+// Set Current Log
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  };
+}
+
+// Clear Current Log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
+  };
 }
